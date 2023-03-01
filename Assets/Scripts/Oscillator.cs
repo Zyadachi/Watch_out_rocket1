720a64f9e29b112f79301202388eb0f3dae24158
Zyadachi <ozdowy.adam8@gmail.com>
2023-03-01 16:53:08 +0100

    Project Files
    added oscillating obstacles to level

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float period = 2f;
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float cycles = Time.time / period;
+        
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) /2f; //recalculated to go from 0 to 1
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
